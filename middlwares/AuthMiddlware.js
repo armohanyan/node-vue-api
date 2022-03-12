fbc582f266500f2dddbc5dd60e70bf52952606b1
ArMinasyan <vip.arman.minasyan.197@gmail.com>
2022-03-12 18:22:55 +0400

    -

--- middlwares/AuthMiddlware.js
+++ middlwares/AuthMiddlware.js
@@ -4,17 +4,16 @@ const requireAuth = (req, res, next) => {
   const token = req?.cookies?.jwt || req?.headers?.authorization?.split(' ')[1] || null;
 
   if(token) {
-    jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
-      if(err) {
-        res.status(401).send('Unauthorized');
-      } else {
-        res.locals.user = decodedToken;
-        next();
-      }
-    });
-  } else {
-    res.status(401).send('Unauthorized');
+    try {
+      const data = jwt.verify(token, process.env.JWT_SECRET, { algorithms: ['HS256'] });
+      res.locals.user = data;
+      next();
+    } catch(err) {
+      res.status(401).send('Unauthorized');
+    }
   }
+  res.status(401).send('Unauthorized');
+
 };
 
 module.exports = { requireAuth };
