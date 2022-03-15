edb70b0a9a1c3b9494c91811d35d172f6c81de03
armohanyyan <ohanyyanarmen@gmail.com>
2022-03-15 17:18:18 +0400

    fix reset password issues, and also add description for routes functions

--- common/token.js
+++ common/token.js
@@ -1,22 +1,28 @@
 const jwt = require('jsonwebtoken');
 
-const token = (payload = {}, secret = process.env.JWT_SECRET, options = {}) => {
-  return jwt.sign(payload, secret, {
-    algorithm: 'HS256',
-    ...options
-  });
-};
+const token = ({ payload = {}, secret = process.env.JWT_SECRET, options = {} }) => {
 
-const verifyToken = (token, secret = process.env.JWT_SECRET, options = {}) => {
-  try {
-    return jwt.verify(token, secret, {
+
+  return jwt.sign(
+    payload,
+    secret, {
       algorithm: 'HS256',
       ...options
-    });
+    }
+  );
+};
+
+const verifyToken = ({ token, secret = process.env.JWT_SECRET, options = {} }) => {
+  try {
+    return jwt.verify(
+      token,
+      secret, {
+        algorithm: 'HS256',
+        ...options
+      });
   } catch(err) {
     return false;
   }
-
 };
 
 module.exports = {
