01cc9ad2f59a4d60d6810f551fb529f4750057e4
armohanyyan <ohanyyanarmen@gmail.com>
2022-03-19 18:42:09 +0400

    fix 3

--- common/validation/ResetPasswordValidation.js
+++ common/validation/ResetPasswordValidation.js
@@ -6,4 +6,8 @@ module.exports = [
     .trim()
     .isLength({ min: 5 })
     .withMessage(validationMessage.min(5))
+    .matches(/^(?=(.*?[A-Z]){3,}).{3,}$/).withMessage("Min 3 uppercase letter")
+    .matches(/^(?=(.*[a-z]){3,}).{3,}$/).withMessage("Min 3 lowercase letter")
+    .matches(/^(?=(.*[\d]){2,}).{2,}$/).withMessage("Min 2 numbers")
+    .matches(/^(?=(.*[\W]){2,}).{2,}$/).withMessage("Min 2 special character")
 ];
